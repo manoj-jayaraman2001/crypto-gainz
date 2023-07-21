a152468fd1cc26708e34b9bfb82a25e2be9742f0
Manoj Jayaraman <manojjayaraman2001@gmail.com>
2023-07-21 09:48:39 +0530

    added coins from api

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,7 +1,96 @@
+import millify from "millify";
+import { useState } from "react";
+import { useGetCryptosQuery } from "../services/createApi";
+import { Card, CardContent, Typography, CardActionArea } from "@mui/material";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import "../styles/cryptocurrencies.css";
 
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data, isFetching } = useGetCryptosQuery();
+  const cryptoCoins = data?.data?.coins;
 
-const Cryptocurrencies = () => {
-    return ( <>CryptoCurrencies</>  );
-}
- 
+  return (
+    <div className="crypto-container">
+      <div className="coin-grid">
+        {cryptoCoins.map((currency, Index) => {
+          return (
+            <CurrencyCard
+              key={Index}
+              id={Index + 1}
+              name={currency.name}
+              imgUrl={currency.iconUrl}
+              price={currency.price}
+              marketCap={currency.marketCap}
+              dailyChange={currency.change}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 export default Cryptocurrencies;
+
+// ---------------Child Components---------------//
+
+const CurrencyCard = ({ id, name, imgUrl, price, marketCap, dailyChange }) => {
+  const fonts = { Lato: "Lato , sans-serif", Karla: "Karla, sans-serif" };
+  return (
+    <Card className="root">
+      <CardActionArea>
+        <CardContent>
+          <div className="card-top">
+            <Typography sx={{ fontFamily: fonts.Lato, fontWeight: 600 }}>
+              {`${id}. ${name}`}
+            </Typography>
+            <img src={imgUrl} alt={`${name}-logo`} className="icon" />
+          </div>
+
+          <div>
+            <Typography
+              sx={{
+                fontFamily: fonts.Karla,
+                fontSize: "2em",
+                color: "#FF630B",
+              }}
+            >
+              {millify(price)}
+            </Typography>
+
+            <div className="info">
+              <Typography sx={{ fontFamily: fonts.Karla, fontSize: "1em" }}>
+                Market Cap:
+              </Typography>
+              <Typography sx={{ fontFamily: fonts.Karla, fontSize: "1em" }}>
+                {millify(marketCap)}
+              </Typography>
+            </div>
+            <div className="info">
+              <Typography sx={{ fontFamily: fonts.Karla, fontSize: "1em" }}>
+                Daily Change:
+              </Typography>
+              <Typography
+                style={{ color: dailyChange > 0 ? "green" : "red" }}
+                sx={{
+                  fontFamily: fonts.Karla,
+                  fontWeight: 600,
+                  fontSize: "1em",
+                }}
+              >
+                {dailyChange}%
+              </Typography>
+              {dailyChange > 0 ? (
+                <ArrowDropUpIcon sx={{ color: "green", fontSize: 30 }} />
+              ) : (
+                <ArrowDropDownIcon sx={{ color: "red", fontSize: "2em" }} />
+              )}
+            </div>
+          </div>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,65 @@
+import { Paper, Grid, Typography } from "@mui/material";
+import { Icon } from "@mui/material";
+import "../styles/home.css";
+import { useGetCryptosQuery } from "../services/createApi";
+import {BsGraphUpArrow, BsCurrencyExchange} from 'react-icons/bs'
+import { Link } from "react-router-dom";
+import {Tb24Hours} from 'react-icons/tb'
+import {GiWorld, GiMoneyStack} from 'react-icons/gi'
+import millify from "millify";
+import {Cryptocurrencies, News} from "../components";
+
 const Home = () => {
-    return ( <>Home</> );
-}
- 
+  const { data, isFetching } = useGetCryptosQuery();
+  const globalStats = data?.data?.stats
+
+  if (isFetching) return 'Loading ...'
+  return (
+    <div className="home-container">
+      <h2>Global Crypto Stats</h2>
+      <div className="card-grid">
+        <StatisticCard title="Total Sales" value={globalStats.total} icon={<BsGraphUpArrow className="icon"/>} />
+        <StatisticCard title="Total Exchanges" value={globalStats.totalExchanges} icon={<BsCurrencyExchange className="icon" />} />
+        <StatisticCard title="Total Market Cap ($)" value={millify(globalStats.totalMarketCap)} icon={<GiMoneyStack className="icon"/>} />
+        <StatisticCard title="Total 24h Volume ($)" value={millify(globalStats.total24hVolume)} icon={<Tb24Hours className="icon"/>} />
+        <StatisticCard title="Total Markets" value={globalStats.totalMarkets} icon={<GiWorld className="icon"/>} />
+      </div>
+      <div className="home-heading-container">
+        <h3>Top 10 Crypto Currencies in the world</h3>
+        <Link to='/cryptocurrencies'>View More</Link>
+      </div>
+      <Cryptocurrencies simplified/>
+      <div className="home-heading-container">
+        <h3>Latest Crypto News</h3>
+        <Link to='/news'>View More</Link>
+      </div>
+      <News simplified/>
+    </div>
+  );
+};
+
 export default Home;
+
+//--------------- Child Components------------//
+
+const StatisticCard = ({ title, value, icon }) => {
+  return (
+    <Paper elevation={3} sx={{ padding: 2, minWidth: 150, maxWidth: 350 }}>
+      <Grid container alignItems="center" spacing={2}>
+        <Grid item>
+          {icon}
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="subtitle1" fontFamily={"Karla"}>
+            {title}
+          </Typography>
+        </Grid>
+        <Grid item xs={7} textAlign="right">
+          <Typography variant="h5" fontWeight="bold" fontFamily={"Lato"}>
+            {value}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
