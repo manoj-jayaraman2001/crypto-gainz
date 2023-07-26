104e99a9a7ffc5d072a2a88e967f93d14f3188dd
Manoj Jayaraman <manojjayaraman2001@gmail.com>
2023-07-26 13:43:29 +0530

    fixing bugs

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,12 +5,8 @@ function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
-    document.documentElement.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "instant", // Optional if you want to skip the scrolling animation
-    });
+    // Scroll to top when the pathname changes
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
   }, [pathname]);
 
   return null;
